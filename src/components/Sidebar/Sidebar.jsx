16992612da7bcfb98a3a399d5bb92bdb1666288e
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-08 18:21:31 +0200

    add categories with icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,6 +13,19 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import useStyles from './styles';
 
+const categories = [
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
+];
+
+const demoCategories = [
+  { label: 'Comedy', value: 'comedy' },
+  { label: 'Action', value: 'action' },
+  { label: 'Horror', value: 'horror' },
+  { label: 'Animation', value: 'animation' },
+];
+
 const redLogo =
   'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 
@@ -32,6 +45,24 @@ const Sidebar = ({ setMobileOpen }) => {
           alt='Filmpire logo'
         />
       </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {demoCategories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to='/'>
+            <ListItem onClick={() => {}} button>
+              <ListItemIcon>
+                <img
+                  src={redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
     </>
   );
 };
