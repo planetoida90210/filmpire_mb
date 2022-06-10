0e81f67bf9f50c1d100c265de8705e4d37004905
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-10 19:24:23 +0200

    redux api setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { tmdbApi } from '../services/TMDB';
 
 export default configureStore({
-  reducers: {},
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
 });
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const tmdbApiKey = process.env.TMDB_KEY;
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3/',
+  }),
+  endpoints: (builder) => ({
+    //* Get Movies by [Type]
+    getMovies: builder.query({
+      query: () => {
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
+  }),
 });
+
+export const { useGetMoviesQuery } = tmdbApi;
