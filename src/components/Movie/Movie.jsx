d3ed5f5d793d0fc74f640fe4ccd364856877f001
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-11 23:30:30 +0200

    rendering photos and titles, add rating to each movie

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -9,10 +9,24 @@ const Movie = ({ movie, i }) => {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} sm={2} md={4} lg={3} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant='h5'>
-        {movie.title}
-      </Typography>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : 'https://www.fillmurray.com/200/300'
+            }
+          />
+          <Typography className={classes.title} variant='h5'>
+            {movie.title}
+          </Typography>
+          <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+        </Link>
+      </Grow>
     </Grid>
   );
 };
