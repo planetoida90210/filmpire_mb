cc4f7a498d877af437bd92eada624e9c8b1478b0
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-14 08:41:02 +0200

    passing user data to profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { userSelector } from '../../features/auth';
 
 const Profile = () => {
-  return <div>profile</div>;
+  const { user: user } = useSelector(userSelector);
+
+  console.log(user);
+  return <div>profile - {user.username}</div>;
 };
 
 export default Profile;
