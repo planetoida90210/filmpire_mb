56728aa3031a18d6c90a1c605c9fbabb69d17c76
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-14 08:22:09 +0200

    auth implementation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,17 +16,22 @@ import {
 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { setUser, userSelector } from '../../features/auth';
 import { Sidebar, Search } from '..';
 import { fetchToken, createSessionId, moviesApi } from '../../utils';
 import useStyles from './styles';
 
 const NavBar = () => {
+  const { isAuthenticated, user } = useSelector(userSelector);
   const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
-  const isAuthenticated = false;
+  const dispatch = useDispatch();
+
+  console.log(user);
 
   const token = localStorage.getItem('request_token');
   const sessionIdFromLocalStorage = localStorage.getItem('session_id');
@@ -38,15 +43,21 @@ const NavBar = () => {
           const { data: userData } = await moviesApi.get(
             `/account?session_id=${sessionIdFromLocalStorage}`
           );
+          // passing user data
+          dispatch(setUser(userData));
         } else {
           const sessionId = await createSessionId();
 
           const { data: userData } = await moviesApi.get(
             `/account?session_id=${sessionId}`
           );
+          //passing user data
+          dispatch(setUser(userData));
         }
       }
     };
+
+    logInUser();
   }, [token]);
   return (
     <>
