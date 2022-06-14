8f9a802a536499cb791aba59f4482ae744539a44
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-14 08:30:49 +0200

    change endpoint of user idurl

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const Profile = () => {
-  return (
-    <div>
-      profile
-      <h1>profile page</h1>
-    </div>
-  );
+  return <div>profile</div>;
 };
 
 export default Profile;
