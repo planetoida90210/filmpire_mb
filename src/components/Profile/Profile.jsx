0d4ef0614300e6d0c8d4210c1a4a0f412a745a13
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-15 22:16:56 +0200

    logout from authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
-  const { user: user } = useSelector(userSelector);
+  const { user } = useSelector(userSelector);
+
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
 
   console.log(user);
-  return <div>profile - {user.username}</div>;
+  return (
+    <Box>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography varian='h4' gutterBottom>
+          My Profile
+        </Typography>
+        <Button color='inherit' onClick={logout}>
+          logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant='h5'>
+          Add favorites or watchlist some movies to see them here
+        </Typography>
+      ) : (
+        <Box>FAVORITE MOVIES</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
