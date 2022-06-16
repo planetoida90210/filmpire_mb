95f5e374328961c3406a30d0715cf207fd63c935
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-16 13:42:06 +0200

    movie information page boilerplate imports, redux endpoit for single movie

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Rating,
+} from '@mui/material';
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+
+import useStyles from './styles';
+import { useGetMovieQuery } from '../../services/TMDB';
 
 const MovieInformation = () => {
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const classes = useStyles();
+
+  if (isFetching) {
+    return (
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <CircularProgress size='8rem' />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Link to='/'>Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+
   return (
-    <div>
-      MovieInformation
-      <h1>movie inf</h1>
-    </div>
+    <Grid container className={classes.containerSpaceAround}>
+      test
+    </Grid>
   );
 };
 
