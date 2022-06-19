f3b8596594859f648e50d9c32454925eefbe8985
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-19 21:43:33 +0200

    implemented add watchlisted and favorited movies to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,18 +4,30 @@ import { useSelector } from 'react-redux';
 import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
+import { useGetListQuery } from '../../services/TMDB';
+import { RatedCards } from '..';
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
-  const favoriteMovies = [];
+  const { data: favoriteMovies } = useGetListQuery({
+    listName: 'favorite/movies',
+    accountId: user.id,
+    sessionId: localStorage.getItem('session_id'),
+    page: 1,
+  });
+  const { data: watchlistMovies } = useGetListQuery({
+    listName: 'watchlist/movies',
+    accountId: user.id,
+    sessionId: localStorage.getItem('session_id'),
+    page: 1,
+  });
 
   const logout = () => {
     localStorage.clear();
     window.location.href = '/';
   };
 
-  console.log(user);
   return (
     <Box>
       <Box display='flex' justifyContent='space-between'>
@@ -26,12 +38,15 @@ const Profile = () => {
           logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      {!favoriteMovies.length ? (
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
         <Typography variant='h5'>
           Add favorites or watchlist some movies to see them here
         </Typography>
       ) : (
-        <Box>FAVORITE MOVIES</Box>
+        <Box>
+          <RatedCards title='Favorite Movies' data={favoriteMovies} />
+          <RatedCards title='Watchlist Movies' data={watchlistMovies} />
+        </Box>
       )}
     </Box>
   );
