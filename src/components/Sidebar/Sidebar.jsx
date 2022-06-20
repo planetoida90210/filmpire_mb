88dff590355bd2b7a3f8bbcf9bfe8a8b7fe4fdde
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-20 16:18:26 +0200

    fixing issue of genre iconchange on dark mode

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -60,7 +60,7 @@ const Sidebar = ({ setMobileOpen }) => {
               <ListItemIcon>
                 <img
                   src={genreIcons[label.toLowerCase()]}
-                  className={classes.genreImages}
+                  className={classes.genreImage}
                   height={30}
                   alt='Genre Icon'
                 />
@@ -87,7 +87,7 @@ const Sidebar = ({ setMobileOpen }) => {
                 <ListItemIcon>
                   <img
                     src={genreIcons[name.toLowerCase()]}
-                    className={classes.genreImages}
+                    className={classes.genreImage}
                     height={30}
                     alt='Genre Icon'
                   />
