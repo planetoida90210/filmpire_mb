77f620de7d24872d3e7eb6b7db3cc321ca167439
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-21 21:15:40 +0200

    implement featured movie card

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -6,7 +6,36 @@ import useStyles from './styles';
 
 const FeaturedMovie = ({ movie }) => {
   const classes = useStyles();
-  return <div>FeaturedMovie</div>;
+
+  if (!movie) return null;
+  return (
+    <Box
+      component={Link}
+      to={`movie/${movie.id}`}
+      className={classes.featuredCardContainer}
+    >
+      <Card className={classes.card} classes={{ root: classes.cardRoot }}>
+        <CardMedia
+          media='picture'
+          alt={movie.title}
+          image={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          title={movie.title}
+          className={classes.cardMedia}
+        />
+        <Box padding='20px'>
+          <CardContent
+            className={classes.cardContent}
+            classes={{ root: classes.cardContentRoot }}
+          >
+            <Typography variant='h5' gutterBottom>
+              {movie.title}
+            </Typography>
+            <Typography variant='body2'>{movie.overview}</Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </Box>
+  );
 };
 
 export default FeaturedMovie;
